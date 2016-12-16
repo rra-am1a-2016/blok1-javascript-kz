b7342e6f254726e140146f43112aca657ff0656b
Arjan de Ruijter <rra@mboutrecht.nl>
2016-12-16 14:59:10 +0100

    Post ajax

--- ajax_intro.js
+++ ajax_intro.js
@@ -17,16 +17,26 @@ xmlhttp.onreadystatechange = function()
    // check de readyState (moet 4 zijn) en de status(moet 200 zijn)
    if (this.readyState == 4 && this.status == 200)
    {
+      //alert(xmlhttp.responseText);
       var jsonObj = JSON.parse(this.responseText);
-      document.getElementById("ajaxTekst").innerHTML = "Mijn naam is: " + jsonObj.name + "<br>Mijn leeftijd is: " + jsonObj.age + "<br>";
+      document.getElementById("ajaxTekst").innerHTML = "Mijn naam is: " + jsonObj.firstname + 
+                                                       "<br>Mijn leeftijd is: " + jsonObj.age + 
+                                                       "<br>Mijn id is: " + jsonObj.id + "<br>";
    }
 };
 
-xmlhttp.open("GET", "data.php", true);
 
-document.getElementById("btn").onclick = function()
-{   
-   xmlhttp.send();
+var divs = document.getElementsByTagName("div");
+
+for ( var i = 0; i < divs.length; i++)
+{
+  divs[i].onmouseover = function()
+  {
+    //alert("Hoi" + this.innerHTML);
+    xmlhttp.open("POST", "data.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("id=" + this.innerHTML);
+  }
 }
 
 
--- data.php
+++ data.php
@@ -2,6 +2,16 @@
    // Opdracht maak een JSON string met daarin een JS object.
    // Dit jsobject heeft een name property met als waarde je voornaam
    
+   // Afvangen geen id meegegeven aan url pagina
+   if (isset($_POST))
+   {
+      $id = $_POST["id"];
+   }
+   else
+   {
+      $id = 1;
+   }
+
    // Dit zijn de servercredentials
    $serverName = "localhost";
    $userName = "root";
@@ -12,7 +22,7 @@
    $conn = mysqli_connect($serverName, $userName, $password, $dbName);
    
    // Maak een zoekvraag
-   $query = "SELECT * FROM `users` WHERE `id` = 2";
+   $query = "SELECT * FROM `users` WHERE `id` = ".$id;
 
    // Verstuur de zoekvraag naar de server
    $result = mysqli_query($conn, $query);
@@ -21,17 +31,9 @@
    // Maar je kunt er wel heel makkelijk een een associatief array van maken. 
    $record = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-   var_dump($record);
+   //var_dump($record);
 
    $jsonStr = json_encode($record);
    echo $jsonStr;
 
-
-
-   $dataArray = array("name" => "Arjan", "age" => 34);
-
-
-
-   $jsonString = json_encode($dataArray);
-   echo $jsonString;
 ?>
